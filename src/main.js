031da73422db65b6650e174fd973ad83c2107709
huangzijian888 <huangzijian888@gmail.com>
2019-09-29 05:16:36 +0800

    完成基本功能

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
+
+import Chat from './components/Chat.vue'
+
+const routes = [{
+	path: '/chat',
+	component: Chat
+}]
+
+const router = new VueRouter({
+	routes
+})
+
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+	
+	router,
+	render: h => h(App),
 }).$mount('#app')
